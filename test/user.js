f4b8b1935afb25a7b580be5743fb65617014166c
vmishra2305 <vishal_mishra@condenast.com>
2023-02-02 16:06:28 +0530

    third

---
+++ test/user.js
@@ -0,0 +1,26 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+const request = supertest('https://gorest.co.in/public/v2/');
+
+const token = 'dfb5b96fb909ee8beb16ac4f2694b4cd30f164c3b41cd6cb07dc4076b5e12091';
+
+describe('Users', ()=>{
+    it(`GET /users`, (done)=>{
+        request.get(`users?access-token=${token}`).end((err,res) => {
+            expect(res.body).to.not.be.empty;
+            done();
+        })
+    })
+
+    it(`GET /users/1`, (done)=>{
+        request.get(`users?gender=female&status=active&access-token=${token}`).end((err,res) => {
+            var counter=1;
+            res.body.forEach(data => {
+                expect(data.gender).to.be.eq('female');
+                expect(data.status).to.be.eq('active');
+                expect(data.id).to.be.eq(5608);
+            });
+            done();
+        })
+    })
+})
