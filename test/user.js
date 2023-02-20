58f78ea7de114a60eecd7d72f5929c968e8e94c5
vmishra2305 <vishal_mishra@condenast.com>
2023-02-20 10:38:32 +0530

    commit01

--- test/user.js
+++ test/user.js
@@ -1,8 +1,9 @@
 import supertest from 'supertest';
 import { expect } from 'chai';
-const request = supertest('https://gorest.co.in/public/v2/');
+require('dotenv').config();
+const request = supertest(process.env.X_URL);
 
-const token = 'dfb5b96fb909ee8beb16ac4f2694b4cd30f164c3b41cd6cb07dc4076b5e12091';
+const token = process.env.X_PASS;
 
 describe('Users', ()=>{
     it(`GET /users`, (done)=>{
@@ -17,7 +18,7 @@ describe('Users', ()=>{
             var counter=1;
             res.body.forEach(data => {
                 expect(data.gender).to.be.eq('female');
-                expect(data.status).to.be.eq('inactive');
+                expect(data.status).to.be.eq('active');
             });
             done();
         })
